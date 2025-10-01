842108881a9a9ceba52f122f8c56eb9711d26888
chenjie <gxuchenjie04@gmail.com>
2025-10-01 20:56:44 +0800

    发布DA3Net

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -68,6 +68,7 @@ from ultralytics.nn.modules import (
     YOLOEDetect,
     YOLOESegment,
     v10Detect,
+    DA3Block,
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, LOGGER, YAML, colorstr, emojis
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
@@ -1700,6 +1701,13 @@ def parse_model(d, ch, verbose=True):
             c2 = args[0]
             c1 = ch[f]
             args = [*args[1:]]
+        #---------------------------------------------------------------------------------------
+        elif m is DA3Block:
+            c2 = args[0]
+            c1 = ch[f]
+            c2 = make_divisible(min(c2, max_channels) * width, 8)
+            args = [c1, c2, *args[1:]]
+        #---------------------------------------------------------------------------------------
         else:
             c2 = ch[f]
 
